4f47d6cdfb9961634f1ea3f6dce195ab7004680b
lEGENDk <legendk@legendk-SUDO.(none)>
2013-07-22 20:24:59 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '7c4d0afca6f0e216e2d48da3b60457e63ccf6fb66a358a731a8bfb585bf886f60d5a7c291556bcf9de9ad7ee94dffd9be290816f9b50509612e6a0402800b1e0'
